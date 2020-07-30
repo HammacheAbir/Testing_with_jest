4fc4af99b262cc03cfdacfb5ff896e253b4d3f2f
Abir <abirhammache@gmail.com>
2020-07-30 00:58:15 -0700

    test updated

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,12 @@ const functions= require ('./functions')
 test('should return the sum', () => {
     expect(functions.add(2,2)).toBe(4)
 })
+
+test('should return null', () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+test('should be falsy', () => {
+    expect(functions.checkValue(0)).toBeFalsy()
+})
+
